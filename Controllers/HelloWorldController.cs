cf2e7718e1d218203eb2c60662d8101740ce4976
Jean Tessier <jean.tessier@forgeglobal.com>
2022-01-24 11:25:50 -0800

    Add parameters to controller action
    Use string interpolation to generate response content.

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -15,10 +15,11 @@ namespace MvcMovie.Controllers
 
         //
         // GET /HelloWorld/Welcome
+        // Requires using System.Text.Encodings.Web;
 
-        public string Welcome()
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
         }
     }
 }
